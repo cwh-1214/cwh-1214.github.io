c9f03990e4851693d71f6eab6b726ae407ea457e
chenwh <431847031@qq.com>
2022-10-16 23:55:43 +0800

    Site updated: 2022-10-16 23:55:34

--- wenhao/random.js
+++ wenhao/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/612eda1d.html","posts/26b560af.html","posts/107dafe.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
+var posts=["posts/4a17b156.html","posts/612eda1d.html","posts/26b560af.html","posts/107dafe.html","posts/28239a8d.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
