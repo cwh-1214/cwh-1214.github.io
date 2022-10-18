2121ccff72ab5424e2de93f042a155e22ed54e9a
chenwh <431847031@qq.com>
2022-10-18 22:21:57 +0800

    Site updated: 2022-10-18 22:21:47

--- wenhao/random.js
+++ wenhao/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/26b560af.html","posts/4a17b156.html","posts/612eda1d.html","posts/107dafe.html","posts/36d3508b.html","posts/28239a8d.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
+var posts=["posts/4a17b156.html","posts/612eda1d.html","posts/26b560af.html","posts/107dafe.html","posts/28239a8d.html","posts/36d3508b.html","posts/bce68613.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
